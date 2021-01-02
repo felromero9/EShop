e10b70380f864c8bfa2504b3f07855aa51cba7b7
Feli <felromero9@gmail.com>
2021-01-02 20:11:56 +0100

    add nodemod currently

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,12 +1,23 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap';
 import Rating from '../components/Rating';
-import products from '../products';
+import axios from 'axios';
 
 const ProductScreen =  ({ match }) => {
-  const product = products.find( p => p.id === match.params.id); //Match the id product with the id <Route path='/product/:id'/>
+  //const product = products.find( p => p.id === match.params.id); //Match the id product with the id <Route path='/product/:id'/>
 
+  const [product, setProduct] = useState({});
+
+  useEffect(() => {
+    const fetchProduct = async () => {
+      const { data } = await axios.get(`/api/products/${match.params.id}`);
+
+      setProduct(data);
+    }
+
+    fetchProduct()
+  }, [match])
 
   return (
     <> 
@@ -55,7 +66,7 @@ const ProductScreen =  ({ match }) => {
             </ListGroup.Item>
 
             <ListGroup.Item>
-              <Button className='btn-block' type='button' disabled={product.countInStock == 0}>
+              <Button className='btn-block' type='button' disabled={product.countInStock === 0}>
                 Add To Card
               </Button>
             </ListGroup.Item>
@@ -67,7 +78,6 @@ const ProductScreen =  ({ match }) => {
     </>
   )
 }
-
 
 
 export default ProductScreen
