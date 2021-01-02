3886e63839b1a93a325cda09c48c833e03c70ef1
Feli <felromero9@gmail.com>
2021-01-02 12:58:44 +0100

    add react-router-bootstrap and react-router-dom

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { Card } from 'react-bootstrap';
 import Rating from './Rating';
 
@@ -6,16 +7,16 @@ import Rating from './Rating';
 const Product = ({ product }) => {
   return (
     <Card className='my-3 p-3 rounded'>
-      <a href={`/product/${product.id}`}>
+      <Link to={`/product/${product.id}`}>
         <Card.Img src={product.image} variant='top' /> 
-      </a>
+      </Link>
 
       <Card.Body className='my-3 p-3 rounded'>
-        <a href={`/product/${product.id}`}>
+        <Link to={`/product/${product.id}`}>
           <Card.Title as='div'> 
             <strong> { product.name } </strong>
           </Card.Title>
-        </a>  
+        </Link>  
         
         <Card.Text as='div'>
           <Rating 
